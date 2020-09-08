01b484fcc084b6ba48953752ed5a762c83d681f2
Yuyu Qian <Yuyu.Qian.Rice@gmail.com>
2020-09-08 15:27:41 -0500

    Updates

---
+++ precache-manifest.34a779f9c245241bece218db052e6fdf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12163dc382fe6a3be21edb10b59381d1",
+    "url": "/mySite/index.html"
+  },
+  {
+    "revision": "63e9d35553b8cad5099f",
+    "url": "/mySite/static/css/main.ec8e1cf0.chunk.css"
+  },
+  {
+    "revision": "23508e7ad575544fe5ec",
+    "url": "/mySite/static/js/2.816dd61c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/mySite/static/js/2.816dd61c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63e9d35553b8cad5099f",
+    "url": "/mySite/static/js/main.8123dd37.chunk.js"
+  },
+  {
+    "revision": "d1d59349101d22a7f5e9",
+    "url": "/mySite/static/js/runtime-main.f3584d78.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/mySite/static/media/logo.ee7cd8ed.svg"
+  }
+]);
